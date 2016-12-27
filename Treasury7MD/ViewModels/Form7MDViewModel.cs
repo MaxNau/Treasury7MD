f115408cdebcb31cb2532a67f996d93a7bc87b9a
Bob <maxnaumovs@gmail.com>
2016-12-27 12:53:44 +0200

    Updated Form7 View, added export to word (docx) functionality - updated NumericTexboxes bindings to update source on LostFocus; - added export to word functionality for kekvs table (testing mode);
    Further improvements on the way:
    - finish and improve export to word functionality;
    - adapt Form7 View for low resolutions;
    - impement autocalculatiing fields for all columns;

--- Treasury7MD/ViewModels/Form7MDViewModel.cs
+++ Treasury7MD/ViewModels/Form7MDViewModel.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Windows.Input;
 using Treasury7MD.Commands;
 using System.Windows;
+using System.IO;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System.Collections.Generic;
 
 namespace Treasury7MD.ViewModels
 {
@@ -19,6 +23,7 @@ namespace Treasury7MD.ViewModels
 
         public ICommand SaveFormCommand { get; set; }
         public ICommand CollapseOrganizationInfoCommand { get; set; }
+        public ICommand ExportToWordDocxCommand { get; set; }
 
         public Form7MDViewModel()
         {
@@ -62,6 +67,183 @@ namespace Treasury7MD.ViewModels
         {
             SaveFormCommand = new MyCommand(SaveForm, CanSaveForm);
             CollapseOrganizationInfoCommand = new MyCommand(CollapseOrganizationInfo, CanCollapseOrganizationInfo);
+            ExportToWordDocxCommand = new MyCommand(ExportToWordDocx, CanExportToWordDocx);
+        }
+
+        private bool CanExportToWordDocx(object obj)
+        {
+            return true;
+        }
+
+        private void ExportToWordDocx()
+        {
+            string filePath = @"C:\Users\Rionis\Downloads\d_10.docx";
+            string file = @"C:\Users\Rionis\Downloads\dd_10.docx";
+            IDictionary<String, BookmarkStart> bookmarkMap = new Dictionary<String, BookmarkStart>();
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            // the code that you want to measure comes here
+
+
+            byte[] byteArray = File.ReadAllBytes(file);
+            File.Copy(filePath, file, true);
+            using (MemoryStream stream = new MemoryStream())
+            {
+                stream.Write(byteArray, 0, (int)byteArray.Length);
+                using (WordprocessingDocument wordDocument = WordprocessingDocument.Open(file, true))
+                {
+                    // Change the document type to Document
+                    wordDocument.ChangeDocumentType(DocumentFormat.OpenXml.WordprocessingDocumentType.Document);
+
+                    // Get the MainPart of the document
+                    MainDocumentPart mainPart = wordDocument.MainDocumentPart;
+
+                    // Get the Document Settings Part
+                    DocumentSettingsPart documentSettingPart1 = mainPart.DocumentSettingsPart;
+
+                    // Create a new attachedTemplate and specify a relationship ID
+                    AttachedTemplate attachedTemplate1 = new AttachedTemplate() { Id = "relationId1" };
+
+                    // Append the attached template to the DocumentSettingsPart
+                    documentSettingPart1.Settings.Append(attachedTemplate1);
+
+                    // Add an ExternalRelationShip of type AttachedTemplate.
+                    // Specify the path of template and the relationship ID
+                    documentSettingPart1.AddExternalRelationship("http://schemas.openxmlformats.org/officeDocument/2006/relationships/attachedTemplate", new Uri(filePath, UriKind.Absolute), "relationId1");
+
+                    // Save the document
+
+
+                    foreach (
+                        BookmarkStart bookmarkStart in
+                        wordDocument.MainDocumentPart.Document.Body.Descendants<BookmarkStart>())
+                    {
+                        // foreach (BookmarkStart bookmarkStart in file.MainDocumentPart.RootElement.Descendants<BookmarkStart>())
+                        //{
+                        bookmarkMap[bookmarkStart.Name] = bookmarkStart;
+
+                    }
+
+                    foreach (BookmarkStart bookmark in bookmarkMap.Values)
+                    {
+                        Run bookmarkText = bookmark.NextSibling<Run>();
+                        if (bookmarkText != null)
+                        {
+                            bookmarkText.GetFirstChild<Text>().Text = "Test";
+                        }
+                    }
+
+                    bookmarkMap["OName"].NextSibling<Run>().GetFirstChild<Text>().Text = "TheCorporation";
+                    bookmarkMap["Territory"].NextSibling<Run>().GetFirstChild<Text>().Text = "Zone51";
+                    bookmarkMap["EDRPOU"].NextSibling<Run>().GetFirstChild<Text>().Text = "123661237";
+                    bookmarkMap["KOTAUU"].NextSibling<Run>().GetFirstChild<Text>().Text = "2212";
+                    bookmarkMap["KOPFG"].NextSibling<Run>().GetFirstChild<Text>().Text = "12";
+
+                    var tables = mainPart.Document.Descendants<Table>().ToList();
+
+                    var rows = tables[2].Elements<TableRow>().Where(row => row.Elements<TableCell>().Any(cell => cell.InnerText == "value")).ToList();
+
+                    int kekvLength = KEKVs.Count;
+
+                    for (int r = 0; r < kekvLength; r++)
+                    {
+
+                        var cells = rows[r].Elements<TableCell>();
+
+
+                        Paragraph p = cells.ElementAt(3).Elements<Paragraph>().First();
+                        Run run = p.Elements<Run>().FirstOrDefault();
+                        if (run != null)
+                        {
+                            Text t = run.Elements<Text>().FirstOrDefault();
+                            t.Text = KEKVs[r].AccountsReceivable.AtTheBeginingOfTheYear.ToString();
+                        }
+
+                        p = cells.ElementAt(4).Elements<Paragraph>().First();
+                        run = p.Elements<Run>().FirstOrDefault();
+                        if (run != null)
+                        {
+                            Text t = run.Elements<Text>().FirstOrDefault();
+                            t.Text = KEKVs[r].AccountsReceivable.AtTheEndOfTheReportingPeriod.ToString();
+                        }
+
+                        p = cells.ElementAt(5).Elements<Paragraph>().First();
+                        run = p.Elements<Run>().FirstOrDefault();
+                        if (run != null)
+                        {
+                            Text t = run.Elements<Text>().FirstOrDefault();
+                            t.Text = KEKVs[r].AccountsReceivable.OverdueAtTheEndOfTheReportingPeriod.ToString();
+                        }
+
+                        p = cells.ElementAt(6).Elements<Paragraph>().First();
+                        run = p.Elements<Run>().FirstOrDefault();
+                        if (run != null)
+                        {
+                            Text t = run.Elements<Text>().FirstOrDefault();
+                            t.Text = KEKVs[r].AccountsReceivable.WrittenOffSinceTheBeginningOfTheYear.ToString();
+                        }
+
+                        p = cells.ElementAt(7).Elements<Paragraph>().First();
+                        run = p.Elements<Run>().FirstOrDefault();
+                        if (run != null)
+                        {
+                            Text t = run.Elements<Text>().FirstOrDefault();
+                            t.Text = KEKVs[r].AccountsPayable.AtTheBeginingOfTheYear.ToString();
+                        }
+
+                        p = cells.ElementAt(8).Elements<Paragraph>().First();
+                        run = p.Elements<Run>().FirstOrDefault();
+                        if (run != null)
+                        {
+                            Text t = run.Elements<Text>().FirstOrDefault();
+                            t.Text = KEKVs[r].AccountsPayable.AtTheEndOfTheReportingPeriod.ToString();
+                        }
+
+                        p = cells.ElementAt(9).Elements<Paragraph>().First();
+                        run = p.Elements<Run>().FirstOrDefault();
+                        if (run != null)
+                        {
+                            Text t = run.Elements<Text>().FirstOrDefault();
+                            t.Text = KEKVs[r].AccountsPayable.OverdueAtTheEndOfTheReportingPeriod.ToString();
+                        }
+
+                        p = cells.ElementAt(10).Elements<Paragraph>().First();
+                        run = p.Elements<Run>().FirstOrDefault();
+                        if (run != null)
+                        {
+                            Text t = run.Elements<Text>().FirstOrDefault();
+                            t.Text = KEKVs[r].AccountsPayable.DebtNotDue.ToString();
+                        }
+
+                        p = cells.ElementAt(11).Elements<Paragraph>().First();
+                        run = p.Elements<Run>().FirstOrDefault();
+                        if (run != null)
+                        {
+                            Text t = run.Elements<Text>().FirstOrDefault();
+                            t.Text = KEKVs[r].AccountsPayable.WrittenOffSinceTheBeginningOfTheYear.ToString();
+                        }
+
+                        p = cells.ElementAt(12).Elements<Paragraph>().First();
+                        run = p.Elements<Run>().FirstOrDefault();
+                        if (run != null)
+                        {
+                            Text t = run.Elements<Text>().FirstOrDefault();
+                            t.Text = KEKVs[r].RegisteredBudgetaryAccountsPayableAtTheEndOfTheReportingPeriod.ToString();
+                        }
+                    }
+                    
+                    
+
+                    mainPart.Document.Save();
+                }
+
+                //      File.WriteAllBytes(file, stream.ToArray());
+            }
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+
+            MessageBox.Show(elapsedMs.ToString());
+
         }
 
         private bool CanCollapseOrganizationInfo(object obj)
